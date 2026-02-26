29b956aca9f85e7bbf972b3c0e989cb1b302191d
Jayant Kumar Sharma <jayant.sharma_cs22@gla.ac.in>
2026-02-26 21:59:53 +0530

    Initial commit

--- server/configs/db.ts
+++ server/configs/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB =async ()=>{
     try{
       mongoose.connection.on('connected',()=>console.log('MongoDB connected'))
-      await mongoose.connect(process.env.MONGODB_URI as string)
+      await mongoose.connect(process.env.MONGODB_URI! as string)
     }catch(error){
       console.error('Error connecting to MongoDB:',error)
     }
--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,54 @@
 import dns from 'node:dns';
 dns.setDefaultResultOrder('ipv4first');
+
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./configs/db.js";
- 
-connectDB();
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
+declare module 'express-session' {
+    interface SessionData {
+        isLoggedIn: boolean;
+        userId: string;
+    }
+}
+
 const app = express();
-
-// Middleware
-app.use(cors())
-app.use(express.json());
-
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true
+}));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    // store: MongoStore.create({
+    //     mongoUrl: process.env.MONGO_URI
+    // })
+}));
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(port, () => {
+            console.log(`Server running at http://localhost:${port}`);
+        });
+    } catch (err) {
+        console.error("Startup error:", err);
+        process.exit(1);
+    }
+};
+
+startServer();
