f2eb1b6547f1cffb7dd588998d2011b1a15b8000
Jayant Kumar Sharma <jayant.sharma_cs22@gla.ac.in>
2026-03-09 20:06:22 +0530

    Initial commit

--- server/server.ts
+++ server/server.ts
@@ -1,54 +1,31 @@
-import dns from 'node:dns';
-dns.setDefaultResultOrder('ipv4first');
-
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
-import connectDB from "./configs/db.js";
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
+import connectDB from "./configs/db";
+import session from "express-session"
 
-declare module 'express-session' {
-    interface SessionData {
-        isLoggedIn: boolean;
-        userId: string;
-    }
+declare module 'express-session'{
+interface SessionData{
+    isLoggedIn:boolean,
+    userID:string
 }
-
+}
+connectDB();
 const app = express();
-const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
-    credentials: true
-}));
-
-app.use(session({
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
-    // store: MongoStore.create({
-    //     mongoUrl: process.env.MONGO_URI
-    // })
-}));
-
+    origin:[' http://localhost:5173/','http://localhost:3000'],
+    credentials:true
+}))
 app.use(express.json());
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 
-const startServer = async () => {
-    try {
-        await connectDB();
-        app.listen(port, () => {
-            console.log(`Server running at http://localhost:${port}`);
-        });
-    } catch (err) {
-        console.error("Startup error:", err);
-        process.exit(1);
-    }
-};
-
-startServer();
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
